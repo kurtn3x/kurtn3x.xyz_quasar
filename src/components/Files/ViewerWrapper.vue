b48380605f533b696235d2e6fcf79a59d70313e3
kurtn3x <kurtn3x@proton.me>
2023-11-15 15:08:57 +0100

    -

--- src/components/Files/ViewerWrapper.vue
+++ src/components/Files/ViewerWrapper.vue
@@ -1,62 +1,105 @@
 <template>
   <q-dialog
     v-model="showDialog"
-    persistent
     :maximized="maximizedToggle"
     transition-show="slide-up"
     transition-hide="slide-down"
+    @hide="close"
   >
-    <q-card>
-      <q-bar>
-        <a
-          :style="maximizedToggle == false ? 'max-width: 250px' : ''"
-          class="ellipsis"
-        >
-          {{ item.name }}
-        </a>
+    <q-card style="min-width: 350px">
+      <q-toolbar class="bg-layout-bg text-layout-text q-pa-none">
+        <q-toolbar-title class="q-ml-md">{{ item.name }}</q-toolbar-title>
         <q-space />
 
         <q-btn
-          dense
           flat
+          stretch
+          size="sm"
           icon="minimize"
           @click="maximizedToggle = false"
           :disable="!maximizedToggle"
         >
-          <q-tooltip v-if="maximizedToggle" class="bg-white text-primary"
-            >Minimize</q-tooltip
-          >
+          <q-tooltip v-if="maximizedToggle">Minimize</q-tooltip>
         </q-btn>
         <q-btn
-          dense
           flat
+          stretch
+          size="sm"
           icon="crop_square"
           @click="maximizedToggle = true"
           :disable="maximizedToggle"
         >
-          <q-tooltip v-if="!maximizedToggle" class="bg-white text-primary"
-            >Maximize</q-tooltip
-          >
+          <q-tooltip v-if="!maximizedToggle">Maximize</q-tooltip>
         </q-btn>
-        <q-btn dense flat icon="close" @click="close">
-          <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+        <q-btn size="sm" flat stretch icon="close" @click="close" class="close">
+          <q-tooltip>Close</q-tooltip>
         </q-btn>
+      </q-toolbar>
+      <q-bar
+        class="q-pa-none"
+        :class="darkmode ? 'bg-grey-7 text-white' : 'bg-grey-4 text-dark'"
+      >
+        <q-btn
+          stretch
+          flat
+          icon="download"
+          :label="'Download (' + item.size + ')'"
+          class="text-weight-bold"
+          @click="downloadFile(item.id)"
+        />
+        <q-separator vertical size="2px" />
       </q-bar>
 
       <q-card-section>
-        <VideoView :prop-item="item" />
+        <VideoView :prop-item="item" v-if="mimePreview.video" />
+        <div v-if="mimePreview.image">
+          <q-img :src="src" />
+        </div>
+        <div v-if="Object.values(mimePreview).every((item) => item === false)">
+          No Preview available.
+        </div>
       </q-card-section>
-      <q-card-section>
-        <q-btn
-          :label="'Download (' + item.size + ')'"
-          icon="file_download"
-          class="cursor-pointer bg-green text-white q-mt-lg"
-          push
-          @click="downloadFile(item.id)"
-          size="xl"
-          style="min-width: 330px"
-        />
-      </q-card-section>
+      <q-separator size="2px" class="q-ma-sm" />
+      <q-card bordered>
+        <q-expansion-item :label="item.name" class="text-h5 text-center">
+          <q-separator />
+          <q-card flat class="bg-transparent">
+            <div class="q-ma-sm">
+              <div class="text-h6 text-left row">
+                <a class="text-weight-bolder col">Name:</a>
+                <a class="col-8 ellipsis">{{ item.name }}</a>
+              </div>
+              <div class="text-h6 text-left row q-mt-md">
+                <a class="text-weight-bolder col">Size:</a>
+                <a class="col-8">{{ item.size }}</a>
+              </div>
+              <div class="text-h6 text-left row q-mt-md">
+                <a class="text-weight-bolder col">ID:</a>
+                <a class="col-8 ellipsis">{{ item.id }}</a>
+              </div>
+              <div
+                class="text-h6 text-left row q-mt-md"
+                style="overflow-wrap: break-word; line-break: anywhere"
+              >
+                <a class="text-weight-bolder col">Path:</a>
+                <a class="col-8 ellipsis">{{ item.path }}</a>
+              </div>
+              <div class="text-h6 text-left row q-mt-md">
+                <a class="text-weight-bolder col">Modified:</a>
+                <a class="col-8 ellipsis">{{ item.modified }}</a>
+              </div>
+              <div class="text-h6 text-left row q-mt-md">
+                <a class="text-weight-bolder col">Created:</a>
+                <a class="col-8 ellipsis">{{ item.created }}</a>
+              </div>
+              <div class="text-h6 text-left row q-mt-md">
+                <a class="text-weight-bolder col">Mime:</a>
+                <a class="col-8 ellipsis">{{ item.mime }}</a>
+              </div>
+            </div>
+          </q-card>
+        </q-expansion-item>
+      </q-card>
     </q-card>
   </q-dialog>
 </template>
@@ -83,6 +126,9 @@ const VIDEOMIME = [
   'audio/wav',
   'application/x-mpegURL',
   'application/dash+xml',
+];
+
+const IMAGEMIME = [
   'image/jpeg',
   'image/jpeg',
   'image/gif',
@@ -90,6 +136,12 @@ const VIDEOMIME = [
   'image/svg+xml',
   'image/webp',
 ];
+
+const TEXTMIME = [''];
+
+const CODEMIME = [''];
+
+const PDFMIME = 'application/pdf';
 
 const VideoView = defineAsyncComponent(() => import('./VideoView.vue'));
 
@@ -107,8 +159,10 @@ export default defineComponent({
     const localStore = useLocalStore();
     const q = useQuasar();
     var item = ref(props.propItem) as Ref<FolderEntryType>;
+    var src = 'https://api.kurtn3x.xyz/files/download/file/' + item.value.id;
     var showDialog = ref(props.active) as Ref<boolean>;
     return {
+      src,
       item,
       localStore,
       q,
@@ -154,6 +208,14 @@ export default defineComponent({
       this.mimePreview.code = false;
       if (VIDEOMIME.includes(mime)) {
         this.mimePreview.video = true;
+      } else if (IMAGEMIME.includes(mime)) {
+        this.mimePreview.image = true;
+      } else if (PDFMIME == mime) {
+        this.mimePreview.pdf = true;
+      } else if (TEXTMIME.includes(mime)) {
+        this.mimePreview.text = true;
+      } else if (CODEMIME.includes(mime)) {
+        this.mimePreview.code = true;
       }
       console.log(this.mimePreview);
     },
@@ -165,3 +227,9 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+.close:hover {
+  background-color: rgba(255, 0, 0, 0.644);
+}
+</style>
